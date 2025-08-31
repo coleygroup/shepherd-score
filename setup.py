78700ddecc9f70d88881d5a8a7175681ed935a00
kentoabeywardane <kento@mit.edu>
2025-08-31 19:01:24 -0400

    Version bump 1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS = [
 
 setup(
     name="shepherd_score",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     install_requires=REQUIREMENTS,  # Add your dependencies here
     author="Kento Abeywardane",
