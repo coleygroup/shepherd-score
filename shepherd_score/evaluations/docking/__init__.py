54bc1b23dbb3105661e5103b6e4c32531cea5b7c
kentoabeywardane <kento@mit.edu>
2025-12-19 14:37:46 -0500

    Refactor docking evals

---
+++ shepherd_score/evaluations/docking/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module contains docking evaluation pipeline and target information.
+"""
+
+from shepherd_score.evaluations.docking.docking import docking_target_info, DockingEvalPipeline
+
+__all__ = [
+    'docking_target_info',
+    'DockingEvalPipeline'
+]
