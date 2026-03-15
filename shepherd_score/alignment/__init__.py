827507a41dbcd728c4476787285fe51b0679af03
kentoabeywardane <kentoabeywardane@gmail.com>
2026-03-15 00:45:11 -0400

    Split up torch analytical grads from normal autograd functions

--- shepherd_score/alignment/__init__.py
+++ shepherd_score/alignment/__init__.py
@@ -10,16 +10,18 @@ from shepherd_score.alignment._torch import (
     objective_pharm_overlay,
     crippen_align,
     optimize_ROCS_overlay,
-    optimize_ROCS_overlay_analytical,
     optimize_ROCS_esp_overlay,
-    optimize_ROCS_esp_overlay_analytical,
     optimize_esp_combo_score_overlay,
     optimize_pharm_overlay,
-    optimize_pharm_overlay_analytical,
     _initialize_se3_params,
     _initialize_se3_params_with_translations,
     _get_45_fibo,
     _quats_from_fibo,
+)
+from shepherd_score.alignment._torch_analytical import (
+    optimize_ROCS_overlay_analytical,
+    optimize_ROCS_esp_overlay_analytical,
+    optimize_pharm_overlay_analytical,
 )
 
 __all__ = [
